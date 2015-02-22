1c3447295b8319c74f78180f96c3cbc62d7efd32
cassidypignatello <cassidy.pignatello@gmail.com>
2015-02-22 18:45:57 -0500

    adds logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/about' => 'pages#about'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   root 'sessions#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
