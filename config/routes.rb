ac3799b79a166f91d8476b520c906659a0d87a29
cassidypignatello <cassidy.pignatello@gmail.com>
2015-02-21 22:57:40 -0500

    adds sessions controller and create login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users
   get '/about' => 'pages#about'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  root 'sessions#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
